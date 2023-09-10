418568f0318a2b65c06e7e8074de7df57846f080
HoCheeWai <ho_chee_wai@rp.edu.sg>
2023-09-10 21:11:54 +0800

    Updated index.html, config.js and Cognito (on AWS, not source code; include comment here for completenetss) to use GitHub serverlerss web host

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
-var homepage = 'http://localhost';
+/* var homepage = 'http://localhost';
 var callback = 'http://localhost/post_login.html';
-var logout_callback = 'http://localhost/post_logout.html';
+var logout_callback = 'http://localhost/post_logout.html'; */
+
+var homepage = 'https://hocheewai.github.io/video_player/';
+var callback = 'https://hocheewai.github.io/video_player/post_login.html';
+var logout_callback = 'https://hocheewai.github.io/video_player/post_logout.html';
